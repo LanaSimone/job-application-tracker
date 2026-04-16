5b9ecdf99ceae791e809009813155e450f804df7
Allana DeCarish <allanadecarishs@gmail.com>
2026-04-16 01:02:35 -0400

    Imporve layout and styling with responsive container and grid

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -107,73 +107,81 @@ function App() {
 
 
   return (
-    <div>
+    <div className="container">
       <h1>Smart Application Assistant</h1>
       <h2>Applications</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-        type="date"
-        name="dateApplied"
-        placeholder="Date Applied"
-        value={formData.dateApplied}
-        onChange={handleInputChange}
-        />
-        <input
-        name="company"
-        placeholder="Company"
-        value={formData.company}
-        onChange={handleInputChange}
-        />
-        <input
-        name="role"
-        placeholder="Position"
-        value={formData.role}
-        onChange={handleInputChange}
-        />
-        <input
-        name="location"
-        placeholder="Location"
-        value={formData.location}
-        onChange={handleInputChange}
-        />
-        <select
-        name="status"
-        value={formData.status}
-        onChange={handleInputChange}
-        >
-          <option value="Applied">Applied</option>
-          <option value="Interviewing">Interviewing</option>
-          <option value="Offer">Offer</option>
-          <option value="Rejected">Rejected</option>
-        </select>
-        <button type="submit">Save</button>
-      </form>
-      <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+      <div className="form-section">
+        <form onSubmit={handleSubmit}>
+          <input
+          type="date"
+          name="dateApplied"
+          placeholder="Date Applied"
+          value={formData.dateApplied}
+          onChange={handleInputChange}
+          />
+          <input
+          name="company"
+          placeholder="Company"
+          value={formData.company}
+          onChange={handleInputChange}
+          />
+          <input
+          name="role"
+          placeholder="Position"
+          value={formData.role}
+          onChange={handleInputChange}
+          />
+          <input
+          name="location"
+          placeholder="Location"
+          value={formData.location}
+          onChange={handleInputChange}
+          />
+          <select
+          name="status"
+          value={formData.status}
+          onChange={handleInputChange}
+          >
+            <option value="Applied">Applied</option>
+            <option value="Interviewing">Interviewing</option>
+            <option value="Offer">Offer</option>
+            <option value="Rejected">Rejected</option>
+          </select>
+          <button type="submit">Save</button>
+        </form>
+      </div>
+      <select
+      className="filter-select"
+      value={filter}
+      onChange={(e) => setFilter(e.target.value)}
+      >
         <option value="All">All</option>
         <option value="Applied">Applied</option>
         <option value="Interviewing">Interviewing</option>
         <option value="Rejected">Rejected</option>
         <option value="Offer">Offer</option>
       </select>
-      {filteredApplications.map((app) => (
-        <div key ={app.id}>
-          <p>Company: {app.company}</p>
-          <p>Role: {app.role}</p>
-          <p>Location:{app.location}</p>
-          <select value={app.status} onChange={(event) => handleStatusChange(app.id, event.target.value)}>
-            <option value="Applied">Applied</option>
-            <option value="Interviewing">Interviewing</option>
-            <option value="Offer">Offer</option>
-            <option value="Rejected">Rejected</option>
-          </select>
-          <p style={{ color: getStatusColor(app.status), fontWeight: "bold" }}>
-            status: {app.status}
-          </p>
-          <p>Date Applied: {app.date_applied}</p>
-          <button onClick={() => handleDelete(app.id)}>Delete</button>
+      <div className="applications-grid">
+          {filteredApplications.map((app) => (
+            <div className="card" key={app.id}>
+              <p>Company: {app.company}</p>
+              <p>Role: {app.role}</p>
+              <p>Location:{app.location}</p>
+              <select value={app.status} onChange={(event) => handleStatusChange(app.id, event.target.value)}>
+                <option value="Applied">Applied</option>
+                <option value="Interviewing">Interviewing</option>
+                <option value="Offer">Offer</option>
+                <option value="Rejected">Rejected</option>
+              </select>
+              <p style={{ color: getStatusColor(app.status), fontWeight: "bold" }}>
+                status: {app.status}
+              </p>
+              <p>Date Applied: {app.date_applied}</p>
+              <button onClick={() => handleDelete(app.id)}>Delete</button>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
   );
 }
 
